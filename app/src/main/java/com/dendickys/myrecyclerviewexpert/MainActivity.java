d697caa98fbf8b12de9a70b90ac3640c36006816
dendickys <febri.unquad2014@gmail.com>
2019-09-25 03:27:09 +0700

    Perbaikan ketika orientasi layar diubah

--- app/src/main/java/com/dendickys/myrecyclerviewexpert/MainActivity.java
+++ app/src/main/java/com/dendickys/myrecyclerviewexpert/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,17 +18,37 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView rvHeroes;
     private ArrayList<Hero> list = new ArrayList<>();
     private String title = "Mode List";
+    private final String STATE_TITLE = "state_title";
+    private final String STATE_LIST = "state_list";
+    private final String STATE_MODE = "state_mode";
+    private int mode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setActionBarTitle(title);
-
         rvHeroes = findViewById(R.id.rv_heroes);
         rvHeroes.setHasFixedSize(true);
 
+        if (savedInstanceState == null) {
+            setActionBarTitle(title);
+            list.addAll(HeroesData.getListData());
+            showRecyclerList();
+            mode = R.id.action_list;
+        } else {
+            title = savedInstanceState.getString(STATE_TITLE);
+            ArrayList<Hero> stateList = savedInstanceState.getParcelableArrayList(STATE_LIST);
+            int stateMode = savedInstanceState.getInt(STATE_MODE);
+
+            setActionBarTitle(title);
+            if (stateList != null) {
+                list.addAll(stateList);
+            }
+            setMode(stateMode);
+        }
+
+        setActionBarTitle(title);
         list.addAll(HeroesData.getListData());
         showRecyclerList();
         showRecyclerGrid();
@@ -38,24 +59,32 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes.setLayoutManager(new LinearLayoutManager(this));
         ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
         rvHeroes.setAdapter(listHeroAdapter);
+
+        /*istHeroAdapter.setOnItemClickCallback(new ListHeroAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Hero data) {
+                showSelectedHero(data);
+            }
+        });*/
     }
 
     private void showRecyclerGrid() {
         rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
         GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
         rvHeroes.setAdapter(gridHeroAdapter);
+
+        /*gridHeroAdapter.setOnItemClickCallback(new GridHeroAdapter.OnItemClickCallback() {
+            @Override
+            public void onItemClicked(Hero data) {
+                showSelectedHero(data);
+            }
+        });*/
     }
 
     private void showRecyclerCardView() {
         rvHeroes.setLayoutManager(new LinearLayoutManager(this));
         CardViewHeroAdapter cardViewHeroAdapter = new CardViewHeroAdapter(list);
         rvHeroes.setAdapter(cardViewHeroAdapter);
-    }
-
-    private void setActionBarTitle(String title) {
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(title);
-        }
     }
 
     @Override
@@ -70,20 +99,43 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(STATE_TITLE, title);
+        outState.putParcelableArrayList(STATE_LIST, list);
+        outState.putInt(STATE_MODE, mode);
+    }
+
     private void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
                 title = "Mode List";
                 showRecyclerList();
                 break;
+
             case R.id.action_grid:
                 title = "Mode Grid";
                 showRecyclerGrid();
                 break;
+
             case R.id.action_cardview:
                 title = "Mode CardView";
                 showRecyclerCardView();
                 break;
         }
+
+        mode = selectedMode;
+        setActionBarTitle(title);
+    }
+
+    private void setActionBarTitle(String title) {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
+    public void showSelectedHero(Hero hero) {
+        Toast.makeText(this, "Kamu memilih " + hero.getName(), Toast.LENGTH_SHORT).show();
     }
 }
